df162692eae5a1f08dc21e99bb64a99c1274bc32
Yiliya <julliet11@mail.ru>
2023-05-02 10:11:28 +0300

    Added Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine ());
+
+int halfNumber = number % 2;
+
+if (halfNumber == 0)
+{
+    Console.WriteLine("Да, число является четным");
+}
+else
+{
+    Console.WriteLine("Нет, число не является четным");
+}
+
